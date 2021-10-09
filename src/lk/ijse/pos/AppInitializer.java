c5947b0c66b02b68a9afbcb835ea6777ee2326f3
tharushika99 <tharushikajpanditha99@gmail.com>
2021-10-09 10:25:25 +0530

    second commit

--- src/lk/ijse/pos/AppInitializer.java
+++ src/lk/ijse/pos/AppInitializer.java
@@ -1,7 +1,14 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) GDSE56-Assignment. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 package lk.ijse.pos;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+
+
 
 public class AppInitializer extends Application {
 
